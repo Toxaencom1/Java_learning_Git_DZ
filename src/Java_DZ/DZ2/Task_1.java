2df9fd5efd20c99d4d1a261436ad387c570b6fd9
Toxaencom1 <Toxaencom@gmail.com>
2023-02-14 21:26:03 +0600

    Add tasks(1,2) for Dz2

---
+++ src/Java_DZ/DZ2/Task_1.java
@@ -0,0 +1,12 @@
+package Java_DZ.DZ2;
+
+//1 . Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки.
+//
+//        Если значение null, то параметр не должен попадать в запрос.
+//
+//        Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+public class Task_1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Java_DZ/DZ2/Task_2.java
@@ -0,0 +1,8 @@
+package Java_DZ.DZ2;
+
+//2 . Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
+public class Task_2 {
+    public static void main(String[] args) {
+
+    }
+}
