e17e4a128044da9869cb66f5919ab1d6d89bfe01
Toxaencom1 <Toxaencom@gmail.com>
2023-02-19 20:16:45 +0600

    Solved task 3 for DZ3

--- src/Java_DZ/DZ3/Task_3.java
+++ src/Java_DZ/DZ3/Task_3.java
@@ -2,8 +2,37 @@ package Java_DZ.DZ3;
 
 //3.Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее арифметическое этого списка.
 
-public class Task_3 {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class Task_3 {
+    public static Integer findMin(List<Integer> list){
+        List<Integer> list_ = new ArrayList<>(list);
+        Collections.sort(list_);
+        return list_.get(0);
+    }
+    public static Integer findMax(List<Integer> list) {
+        List<Integer> list_ = new ArrayList<>(list);
+        Collections.sort(list_);
+        return list_.get(list_.size() - 1);
+    }
+
+    public static double findAverage(List<Integer> list) {
+        double average = 0;
+        for (Integer value : list) average += value;
+        average = average/list.size();
+        return average;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> randomList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество элементов списка: ");
+        int n = scanner.nextInt();
+        scanner.close();
+        Random random = new Random();
+        for (int i = 0; i < n; i++) randomList.add(random.nextInt(0, 100));
+        System.out.println(randomList+" - Целочисленный список");
+        System.out.println(findMin(randomList) + " - Минимальное значение списка");
+        System.out.println(findMax(randomList)+ " - Максимальное значение списка");
+        System.out.println(findAverage(randomList)+ " - Среднее арифметическое значение списка");
     }
 }
