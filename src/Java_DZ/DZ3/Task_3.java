436924d87d4e33a98e21af2043bf64fe2fb5d693
Toxaencom1 <Toxaencom@gmail.com>
2023-02-19 19:39:37 +0600

    Add task 3 for DZ3

---
+++ src/Java_DZ/DZ3/Task_3.java
@@ -0,0 +1,9 @@
+package Java_DZ.DZ3;
+
+//3.Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее арифметическое этого списка.
+
+public class Task_3 {
+    public static void main(String[] args) {
+
+    }
+}
