77cebdcf5a9de5400ea7dc033c1d0d9841f297fe
Toxaencom1 <Toxaencom@gmail.com>
2023-02-24 19:49:47 +0600

    Add first task for DZ5

---
+++ src/Java_DZ/DZ5/Task_1.java
@@ -0,0 +1,8 @@
+package Java_DZ.DZ5;
+//Реализуйте структуру телефонной книги с помощью HashMap, учитывая,
+// что 1 человек может иметь несколько телефонов.
+public class Task_1 {
+    public static void main(String[] args) {
+
+    }
+}
